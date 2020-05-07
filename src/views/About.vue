bd11ea80d1842871f67db5239d82ed0a5e67e612
Dane <speakdane@gmail.com>
2020-05-07 19:56:20 +0200

    about edit with images attempt circular image

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img alt="Vue logo" src="../assets/dane.jpg">
+    <div>
+      <b-img src="../assets/dane.jpg" v-bind="mainProps" rounded="circle" alt="Circle image"></b-img>
+    </div>
   </div>
 </template>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+export default {
+  data () {
+    return {
+      mainProps: { blank: true, width: 75, height: 75, class: 'm1' }
+    }
+  }
+}
+</script>
 <style scoped lang="scss">
 </style>
