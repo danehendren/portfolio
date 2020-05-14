5060e382d4ac14823b2a43185b783b0e393bbc42
Joseph Kirby <Joseph.Kirby@hcahealthcare.com>
2020-05-14 01:45:38 -0400

    get Contentful images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row" v-if="contentfulImages">
+      <!-- all assets src path -->
+      <img class="portfolio-image" v-for="image in contentfulImages" :key="image.id" :src="image.fields.file.url" alt="">
+
+      <!-- portfolioImages content type src path -->
+      <!-- <img class="portfolio-image" v-for="image in contentfulImages" :key="image.id" :src="image.fields.image.fields.file.url" alt=""> -->
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  props: {
+    contentfulImages: {
+      type: Array,
+      default: null
+    }
+  },
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      images: null
+    }
+  },
+  mounted () {
+    console.log('contentfulImages', this.contentfulImages)
+  },
+  methods: {
+  },
+  watch: {
+    contentfulImages () {
+      console.log('contentfulImages', this.contentfulImages)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.portfolio-image {
+  height: 300px;
+  width: 300px;
+}
 </style>
