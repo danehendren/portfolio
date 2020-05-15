26bb11755b292e273a40fbc16ffb4b894fa25db1
Dane <speakdane@gmail.com>
2020-05-15 20:27:52 +0200

    added things for about AND contact albeit smaller things

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,13 @@
   <b-container>
     <b-row align-v="center" align-h="center">
       <div class="about">
-        <h1 class="mt-3">This is an about page</h1>
-        <b-img alt="Profile Image" class="profile mt-5" fluid rounded="circle" thumbnail :src="require('../assets/ira.jpg')"></b-img>
+        <!-- <h1 class="mt-3">Optional Title</h1> -->
+        <b-img alt="Profile Image" class="profile mt-5" fluid rounded="circle" thumbnail :src="require('../assets/tester.jpg')"></b-img>
         <div class="m-3">
           <h1>Irina Schaefer</h1>
-          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          <p class="mx-auto about-description" style="max-width: 800px;">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
           <br>
-          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          <p class="mx-auto about-description" style="max-width: 800px;">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
         </div>
       </div>
     </b-row>
@@ -27,5 +27,9 @@ export default {
 .profile{
   height: auto;
   width: 50%;
+}
+.about-description{
+  line-height: 2.50rem;
+  font-size: 1.50rem;
 }
 </style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-    <h1>Contact Me in the Views</h1>
-  </div>
+  <b-container>
+    <b-row align-v="center" align-h="center">
+      <div class="about">
+        <b-img alt="Profile Image" class="profile mt-5" fluid rounded="Rounded image" thumbnail :src="require('../assets/foxsit.jpg')"></b-img>
+        <div class="m-3">
+          <!-- <h1 v-on:click="emailMe">Email Me</h1> -->
+          <b-button v-b-tooltip.hover.right="'Clicking will open your email client'" variant="outline-success" v-on:click="emailMe">Click To Email Me</b-button>
+          <p class="mx-auto about-description" style="max-width: 800px;">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+        </div>
+      </div>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {
-  name: 'Contact',
-  components: {}
+  data () {
+    return {
+      mainProps: { blank: true, width: 75, height: 75, class: 'm1' }
+    }
+  },
+  methods: {
+    emailMe: function () {
+      window.location.href = 'mailto:irinaschaefer.4@gmx.de'
+    }
+  }
 }
+// Regular expression from W3C HTML5.2 input specification:
+// https://www.w3.org/TR/html/sec-forms.html#email-state-typeemail
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.profile{
+  height: auto;
+  width: 30%;
+}
+.about-description{
+  line-height: 2.50rem;
+  font-size: 1.50rem;
+}
 </style>
